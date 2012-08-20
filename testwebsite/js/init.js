4025f0f031852b264c0b3411af92153d65b80a9a
CLOCK <zcf1250@muyingzhijia.com>
2012-08-20 16:39:37 +0800

    0820-1
    1、整理css，分成reset.css,base.css,index.css.category.css,map.css
    2、新增网站地图页面sitemap.html
    reset.css是css重置文件，base.css存放的是头尾和全站共用的一些样式，这两个文件每个页面都要引用。

---
+++ testwebsite/js/init.js
@@ -0,0 +1,21 @@
+
+define(function(require) {	
+	
+
+	var $ = jQuery = require('jquery');
+	var Backbone = require('backbone');
+	var _ = require('underscore');
+	
+
+
+	$(function() {				
+		var BBRouter = require('./myapp/bbrouter');
+        BBRouter.initialize();
+
+	});
+	
+	
+
+});
+
+
