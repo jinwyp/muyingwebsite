f5c21a15aae26d638e5c79e34fe4e8893ec15e51
张建强 <jankergg@gmail.com>
2013-01-08 14:43:02 +0800

    dialog组件更新；加入注释

--- website/js/my/dialog.js
+++ website/js/my/dialog.js
@@ -1,31 +1,20 @@
- //png透明插件,支持png背景和png文件
- (function($){
-     $.fn.pngfix = function(options) {
-         var defualts = {scale:"scale"/*scale拉伸；nocrop不拉伸*/,tranceImg:"images/trance.gif",ie6:($.browser.msie&&($.browser.version=="6.0")&&!$.support.style)},opts = $.extend({}, defualts, options);
-         if(!opts.ie6){return}
-         $.each($(this),function(){
-             var oo = $(this),ow=oo.width(),oh=oo.height(),png=(oo[0].nodeName==="IMG")?oo.attr("src"):oo.css("background-image").split('("')[1].split('")')[0];
-             if(oo[0].nodeName==="IMG"){oo.attr("src",opts.tranceImg).width(ow).height(oh).css({background:'none',filter:'progid:DXImageTransform.Microsoft.AlphaImageLoader(src="'+png+'",sizingMethod="'+opts.scale+'")'});}
-             else{oo.css({background:'none',position:'relative',cursor:function(){return (oo[0].nodeName==="A")?'pointer':'default'}});
-                 var odiv = $("<span></span>").prependTo(oo).css({background:'none',filter:'progid:DXImageTransform.Microsoft.AlphaImageLoader(src="'+png+'",sizingMethod="'+opts.scale+'")',width:ow,height:oh,position:'absolute',left:0,top:0})
-             }
-         })
-     }
- })(jQuery);
-
- //dialog
+/**
+ * Created with JetBrains WebStorm.
+ * User: Janker Zhang
+ * Date: 13-01-08
+ * Time: 下午2:16
+ * Dialog 对话框组件
+ */
  function Dialog(options){
-     this.showclose = options.showclose;
-     this._class = !!options._class?options._class:"";
-     this.png_fix = !!options.png_fix;
-     this.title = !!options.title?options.title:"";
-     this.border = options.border;
-     this.HTML = !!options.HTML?options.HTML:"";
-     this.closeEvent = !!options.closeEvent?options.closeEvent:undefined;
-     this.showEvent = !!options.showEvent?options.showEvent:undefined;
-     this.useAjax = !!options.useAjax;
-
-     var _this = this;
+     this.showclose = options.showclose;//是否显示关闭按钮
+     this._class = !!options._class?options._class:"";//弹出框样式class 参见dialog.css
+     this.png_fix = !!options.png_fix;//是否加png半透明边框
+     this.title = !!options.title?options.title:"";//标题
+     this.border = options.border;//边框，为opacity透明度0-1
+     this.useAjax = !!options.useAjax;//是否启用ajax
+     this.HTML = !!options.HTML?options.HTML:"";//加载HTML或者加载html模板
+     this.closeEvent = !!options.closeEvent?options.closeEvent:undefined;//关闭对话框里的事件，无阻断
+     this.showEvent = !!options.showEvent?options.showEvent:undefined;//对话框显示后触发的事件
 
      if(!$("#dialog").length){
          $("<div id='dialog'><div class='dialog-content'><div class='title'>"+this.title+"<a href='#' class='close'></a></div><div class='gost-content'></div></div></div>").appendTo("body")
@@ -44,7 +33,7 @@
          }).appendTo("body")
      }
 
-     var $this = $("#dialog").addClass(this._class),
+     var $this = $("#dialog").addClass(this._class), _this = this,
          content = $this.find(".dialog-content"),
          topcon = $('<div class="top-wrap"><div class="pngfix ctl"></div><div class="pngfix fixwidth ctm"></div><div class="pngfix ctr"></div></div>'),
          btncon = $('<div class="btm-wrap"><div class="pngfix cbl"></div><div class="pngfix fixwidth cbm"></div><div class="pngfix cbr"></div></div>');
@@ -63,24 +52,13 @@
          $this.find(".pngfix").pngfix()
      }
 
-     var cw = content.width(),
-         ch = content.height(),
-         clw = $this.find('.ctl').width(),
-         crw = $this.find('.ctr').width(),
-         dw = $this.width(clw+cw+crw),
-         dh = $this.height();
+     var cw = content.width(), ch = content.height(), clw = $this.find('.ctl').width(), crw = $this.find('.ctr').width(), dw = $this.width(clw+cw+crw), dh = $this.height();
 
      $this.find(".fixwidth").width(cw);
      $this.find(".fixheight").height(ch);
      $this.css({
          "margin-left":-dw.width()/2,
          top:($(window).height()-dh)/2-50
-     });
-
-     $this.find(".close").click(function(){
-         $this.hide();
-         cover.hide();
-         return false
      });
 
      if(!!this.border && typeof this.border==="number"){
@@ -127,7 +105,7 @@
          $this.find(".fixheight").height(ch);
          $this.css({
              "margin-left":-dw.width()/2,
-             top:($(window).height()-dh)/2-50
+             top:($(window).height()-dh)/2
          });
      }
 
@@ -142,3 +120,17 @@
      }
  }
 
+ //png透明插件,支持png背景和png文件
+ (function($){
+     $.fn.pngfix = function(options) {
+         var defualts = {scale:"scale"/*scale拉伸；nocrop不拉伸*/,tranceImg:"images/trance.gif",ie6:($.browser.msie&&($.browser.version=="6.0")&&!$.support.style)},opts = $.extend({}, defualts, options);
+         if(!opts.ie6){return}
+         $.each($(this),function(){
+             var oo = $(this),ow=oo.width(),oh=oo.height(),png=(oo[0].nodeName==="IMG")?oo.attr("src"):oo.css("background-image").split('("')[1].split('")')[0];
+             if(oo[0].nodeName==="IMG"){oo.attr("src",opts.tranceImg).width(ow).height(oh).css({background:'none',filter:'progid:DXImageTransform.Microsoft.AlphaImageLoader(src="'+png+'",sizingMethod="'+opts.scale+'")'});}
+             else{oo.css({background:'none',position:'relative',cursor:function(){return (oo[0].nodeName==="A")?'pointer':'default'}});
+                 var odiv = $("<span></span>").prependTo(oo).css({background:'none',filter:'progid:DXImageTransform.Microsoft.AlphaImageLoader(src="'+png+'",sizingMethod="'+opts.scale+'")',width:ow,height:oh,position:'absolute',left:0,top:0})
+             }
+         })
+     }
+ })(jQuery);
