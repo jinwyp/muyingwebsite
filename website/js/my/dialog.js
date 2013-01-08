05ce89371d6215fc5fda48bedc85344650639a6b
张建强 <jankergg@gmail.com>
2013-01-08 13:39:59 +0800

    按钮显示兼容性修改；dialog组件更新

--- website/js/my/dialog.js
+++ website/js/my/dialog.js
@@ -15,12 +15,17 @@
 
  //dialog
  function Dialog(options){
-     this.showclose = !!options.showclose;
+     this.showclose = options.showclose;
      this._class = !!options._class?options._class:"";
      this.png_fix = !!options.png_fix;
      this.title = !!options.title?options.title:"";
-     this.trancebord = !!options.trancebord;
+     this.border = options.border;
      this.HTML = !!options.HTML?options.HTML:"";
+     this.closeEvent = !!options.closeEvent?options.closeEvent:undefined;
+     this.showEvent = !!options.showEvent?options.showEvent:undefined;
+     this.useAjax = !!options.useAjax;
+
+     var _this = this;
 
      if(!$("#dialog").length){
          $("<div id='dialog'><div class='dialog-content'><div class='title'>"+this.title+"<a href='#' class='close'></a></div><div class='gost-content'></div></div></div>").appendTo("body")
@@ -33,7 +38,7 @@
              opacity:0.6,
              "z-index":"998",
              position:"absolute",
-             background:"#333333",
+             background:"#333",
              top:0,
              left:0
          }).appendTo("body")
@@ -46,7 +51,7 @@
      content.wrap('div.con-wrap').before(topcon).after(btncon);
      content.before('<div class="pngfix fixheight cml"></div>').after('<div class="pngfix fixheight cmr"></div>');
 
-     if(!this.showclose){
+     if(this.showclose===false){
          $this.find(".close").remove()
      }
 
@@ -56,14 +61,6 @@
 
      if(this.png_fix){
          $this.find(".pngfix").pngfix()
-     }
-
-     if(this.trancebord){
-         $this.find(".pngfix").css("opacity",0.5)
-     }
-
-     if(this.HTML){
-         $this.find(".gost-content").html(this.HTML)
      }
 
      var cw = content.width(),
@@ -86,13 +83,62 @@
          return false
      });
 
+     if(!!this.border && typeof this.border==="number"){
+         $this.find(".pngfix").css("opacity",this.border)
+     }
 
+     if(this.HTML){
+         if(this.useAjax){
+             $.ajax({
+                 url:this.HTML,
+                 dataType:"html",
+                 success:function(data){
+                     $this.find(".gost-content").html(data)
+                 },
+                 complete:function(){
+                     if(_this.showEvent){
+                         _this.showEvent()
+                     }
+                     fixit()
+                 },
+                 fail:function(){
+                     $this.find(".gost-content").text("对不起，出错了")
+                 }
+             })
+         }else{
+             $this.find(".gost-content").html(this.HTML)
+         }
 
+     }
 
-     $this.find("#btn").click(function(){
-         $this.animate({top:0},250,function(){$this.hide()});
+     $this.find(".close").click(function(){
+         if(!!_this.closeEvent){_this.closeEvent()}
+         thisClose();
+         return false
      });
 
-     return $this
+     if(!!this.showEvent){
+         this.showEvent()
+     }
+
+     function fixit(){
+         var cw = content.width(), ch = content.height(), clw = $this.find('.ctl').width(), crw = $this.find('.ctr').width(), dw = $this.width(clw+cw+crw), dh = $this.height();
+         $this.find(".fixwidth").width(cw);
+         $this.find(".fixheight").height(ch);
+         $this.css({
+             "margin-left":-dw.width()/2,
+             top:($(window).height()-dh)/2-50
+         });
+     }
+
+     function thisClose(){
+         $this.fadeOut(200,function(){$this.remove()});
+         cover.fadeOut(300,function(){cover.remove()});
+     }
+
+     return {
+         close : thisClose,
+         fixit:fixit()
+     }
  }
 
