ddf92ea219d697e7ec1fe317f19f4e798e49db51
Jin <jinwyp@gmail.com>
2013-02-04 19:25:03 +0800

    增加商品数量

--- nodejs/email/public/js/my/emailfront.js
+++ nodejs/email/public/js/my/emailfront.js
@@ -128,13 +128,34 @@ head.ready(function () {
         productmarketprice: 20,
         productfinalprice:10
     });
+	
+    app.m.product5 = new app.model.Product({
+        productid:123102233,
+        productname: "皇室新小888蜂4444",
+        productintro :"早期感官发育 适合0岁以上宝宝",
+        producturl : "http://www.muyingzhijia.com/shopping/productdetail.aspx?pdtID=81360&ctlid=433&utm_source=myzj&utm_medium=email&utm_term=hv&utm_campaign=toys0123" ,
+        productpic : "http://img.muyingzhijia.com/edm/2013/0122toys/images/baby_joy_confluence_edm0121_10.jpg",
+        productmarketprice: 20,
+        productfinalprice:10
+    });
 
+    app.m.product6 = new app.model.Product({
+        productid:1231033312123,
+        productname: "皇室新77",
+        productintro :"早期感官发育 适合0岁以上宝宝",
+        producturl : "http://www.muyingzhijia.com/shopping/productdetail.aspx?pdtID=81360&ctlid=433&utm_source=myzj&utm_medium=email&utm_term=hv&utm_campaign=toys0123" ,
+        productpic : "http://img.muyingzhijia.com/edm/2013/0122toys/images/baby_joy_confluence_edm0121_10.jpg",
+        productmarketprice: 20,
+        productfinalprice:10
+    });
+	
     app.co.plist1 = new app.collection.Productlist();
     app.co.plist1.add(app.m.product1);
     app.co.plist1.add(app.m.product2);
     app.co.plist1.add(app.m.product3);
     app.co.plist1.add(app.m.product4);
-
+    app.co.plist1.add(app.m.product5);
+    app.co.plist1.add(app.m.product6);
 
     app.m.email1 = new app.model.Email({
         emailid:null,
@@ -153,7 +174,6 @@ head.ready(function () {
         bottempic4 : 'http://img.muyingzhijia.com/edm/2013/0122toys/images/baby_joy_confluence_edm0121_71.jpg',
         couponcode : 'QHUYLP',
         coupondate : '在活动价基础上抵用(有效期截至：2013/1/31)'
-
     });
     app.v.email1 = new app.view.Email({ model: app.m.email1, collection: app.co.plist1, el:'body' });
 
